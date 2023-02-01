0aeb5004eff50e76695f6c852715ecd08cab3d91
TruongL <5490853+qcbit@users.noreply.github.com>
2023-02-01 08:58:20 -0600

    Added code to handle GitReleaseManager

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,11 @@ LABEL version="0.1.0"
 LABEL repository="https://github.com/qcbit/auto-release-milestone"
 LABEL maintainer="Lance Truong"
 
+RUN apt-get update && apt-get install -y jq
+RUN dotnet tool install -g GitReleaseManager.Tool
+
+ENV PATH /root/.dotnet/tools:$PATH
+
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,31 @@
 # Warn if undeclared variable is used
 set -u 
 
-echo "release-url=http://example.com" >> $GITHUB_OUTPUT
+repo_token=$1
+
+if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
+    echo "::debug::The event name was '$GITHUB_EVENT_NAME'"
+    exit 0
+fi
+
+event_type=$(jq --raw-output .action $GITHUB_EVENT_PATH)
+
+if [ $event_type != "close" ]; then
+    echo "::debug::The event type was '$event_type'"
+    exit 0
+fi
+
+milestone_name=$(jq --raw-output .milestone.title $GITHUB_EVENT_PATH)
+
+IFS='/' read owner repository <<< "$GITHUB_REPOSITORY"
+
+release_url=$(dotnet gitreleasemanager create \
+--milestone $milestone_name \
+--targetcommitish $GITHUB_SHA \
+--token $repo_token \
+--owner $owner \
+--repository $repository)
+
+echo "release-url=$release_url" >> $GITHUB_OUTPUT
 
 exit 0
